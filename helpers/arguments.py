a3397710a563f3f27727f166f9340f5c9f0b89b3
Diego Souza <souza.claudiodiego@gmail.com>
2023-10-23 19:57:17 -0300

    arguments

--- helpers/arguments.py
+++ helpers/arguments.py
@@ -23,27 +23,24 @@ def arguments():
     parser.add_argument('--rho',
                         type=int,
                         default=100,
-                        help='Number of tokens to be created per second')
+                        help='Number of packets that can be sent each second. This value will be multiplied by mtu '
+                             'since 1 byte consumes 1 token')
 
     parser.add_argument('--sigma',
                         type=int,
                         default=100,
-                        help='Max capacity of the bucket (also burst size) in bytes')
+                        help='The burst size and the bucket capacity, in packets. This value will be multiplied by mtu '
+                             'since 1 byte consumes 1 token')
 
     parser.add_argument('--queue_capacity',
                         type=int,
-                        default=999999999999999999999999,
-                        help='Max capacity of the queue in bytes')
+                        default=9999999999999999,
+                        help='Max capacity of the queue in packets')
 
     parser.add_argument('--delay_sla',
                         type=float,
                         default=0.01,
                         help='The SLA delay')
-
-    parser.add_argument('--max_time',
-                        type=float,
-                        default=300,
-                        help='Maximum program execution time (in seconds)')
 
     parser.add_argument('--mtu',
                         type=int,
@@ -55,6 +52,11 @@ def arguments():
                         default=100,
                         help='The percentage of the calculated rate to be used')
 
+    parser.add_argument('--max_time',
+                        type=float,
+                        default=300,
+                        help='Maximum program execution time (in seconds)')
+
     parser.add_argument('--sampling_interval',
                         type=float,
                         default=0.001,
@@ -65,4 +67,12 @@ def arguments():
                         default=60,
                         help='Window to create a new set of sampling files')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    args.rho = args.rho * args.mtu
+
+    args.sigma = args.sigma * args.mtu
+
+    args.queue_capacity = args.queue_capacity * args.mtu
+
+    return args

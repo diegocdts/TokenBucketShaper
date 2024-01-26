4a8443a62f5b4cf9b6d3c02de157c6c5ec8869da
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-26 13:01:34 -0300

    main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import simpy
 
 from helpers.arguments import arguments
 from helpers.instances import get_transmission_queue, get_token_buckets, get_flows, get_uflows
-from helpers.plots import samplings_as_png, token_buckets_shaper_occupation, plot_parameters_analysis
-from helpers.processes import refill_tokens, sampling
+from helpers.plots import samplings_as_png, token_buckets_shaper_occupation, plot_parameters_analysis, samplings_as_csv
+from helpers.processes import refill_tokens
 
 if __name__ == "__main__":
     # arguments parse
@@ -25,14 +25,12 @@ if __name__ == "__main__":
     env.process(refill_tokens(env, mtu=args.mtu, tokens_per_second=args.rho, token_buckets=flows.pre_token_buckets))
     env.process(refill_tokens(env, mtu=args.mtu, tokens_per_second=args.rho, token_buckets=token_buckets))
 
-    # samples information from queue_node
-    env.process(sampling(env, args, simulation_info, queue_nodes))
-
     # runs the simpy processes
     env.run(until=args.max_time)
 
     # plot the occupation and latency from the queue_node, the shaper occupation from the token_buckets, and
     # the occupation from the queue_node based on the rho and sigma parameters
+    samplings_as_csv(simulation_info, queue_nodes)
     samplings_as_png(args, simulation_info, queue_nodes)
     token_buckets_shaper_occupation(token_buckets, simulation_info)
     plot_parameters_analysis(simulation_info)

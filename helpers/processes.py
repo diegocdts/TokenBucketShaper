1f38002c94709cce51ac67d0c019ebd5fce86c3a
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-26 13:00:27 -0300

    processes

--- helpers/processes.py
+++ helpers/processes.py
@@ -1,19 +1,3 @@
-from helpers.plots import samplings_as_csv
-
-
-def sampling(env, args, simulation_info, queue_nodes):
-    for node in queue_nodes:
-        samplings_as_csv(node.id, simulation_info)
-    while True:
-        print('timestamp: {:.3f}'.format(env.now))
-        yield env.timeout(args.sampling_interval)
-        for node in queue_nodes:
-            occupancies = node.occupancies
-            latencies = node.latencies
-            node.restart_samplers()
-            samplings_as_csv(node.id, simulation_info, occupancies, latencies)
-
-
 def refill_tokens(env, mtu, tokens_per_second, token_buckets):
     while True:
         yield env.timeout(mtu / tokens_per_second)

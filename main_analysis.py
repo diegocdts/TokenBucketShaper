4ba90a3c1f5842bb5f785fee3dd53e9d38469d8e
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-07 16:02:33 -0300

    main_analysis

--- main_analysis.py
+++ main_analysis.py
@@ -1,3 +1,5 @@
+import os
+
 from analysis.data_analysis import Analysis
 from analysis.data_visualization import Visualization
 from helpers.outputs import Metric
@@ -6,11 +8,14 @@ from helpers.outputs import Metric
 def run(experiment_name, node, metric):
     analysis = Analysis()
     analysis.load_data(experiment_name, node, metric)
-    best = analysis.best_distribution()
-    analysis.sort_distributions_by_aic()
+    analysis.score_distributions()
+    sorted_distributions = analysis.sort_distributions_by_aic()
 
     visualization = Visualization(analysis.data, metric)
-    visualization.histogram(best, True)
+    visualization.histogram(experiment_name, sorted_distributions[:3], True)
 
 
-run('rate_12.75MBps|flows_100|lambda_1010|rho_128.00KB|sigma_384B|SLA_0.01s', 0, Metric.occupancy)
+scenarios = [scenario for scenario in sorted(os.listdir('outputs/')) if scenario.startswith('rate')]
+
+for scenario in scenarios:
+    run(scenario, 0, Metric.occupancy)

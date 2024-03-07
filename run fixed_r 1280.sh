0093bf4fb9444a3227b3cc5d6c87717f17424ad1
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-07 12:16:09 -0300

    run.sh new

---
+++ run fixed_r 1280.sh
@@ -0,0 +1,12 @@
+python main.py --fixed_rate 12800000 --rho 1001 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1002 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1003 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1004 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1005 --sigma 3 --lambda_param 1010
+
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 4 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 5 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 6 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 7 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 8 --lambda_param 1010

4ea296191d50e0df400893b3b57c53e2c29a1a5d
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-08 16:03:41 -0300

    run.sh 1280

--- run fixed_r 1280.sh
+++ run fixed_r 1280.sh
@@ -1,12 +1,11 @@
-python main.py --fixed_rate 12800000 --rho 1001 --sigma 3 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1002 --sigma 3 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1003 --sigma 3 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1004 --sigma 3 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1005 --sigma 3 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 3 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1250 --sigma 3 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1500 --sigma 3 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1750 --sigma 3 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 2000 --sigma 3 --lambda_param 2000
 
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 3 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 4 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 5 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 6 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 7 --lambda_param 1010
-python main.py --fixed_rate 12800000 --rho 1000 --sigma 8 --lambda_param 1010
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 3 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 10 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 15 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 20 --lambda_param 2000
+python main.py --fixed_rate 12800000 --rho 1000 --sigma 25 --lambda_param 2000

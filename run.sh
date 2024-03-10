09bb25cf25da795db149d13d4661685c9196dab3
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-10 11:39:46 -0300

    run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+python main.py --sigma 1 --lambda_plus_sigma True
+python main.py --sigma 2 --lambda_plus_sigma True
+python main.py --sigma 3 --lambda_plus_sigma True
+python main.py --sigma 4 --lambda_plus_sigma True
+python main.py --sigma 5 --lambda_plus_sigma True
+python main.py --sigma 6 --lambda_plus_sigma True
+python main.py --sigma 7 --lambda_plus_sigma True
+python main.py --sigma 8 --lambda_plus_sigma True
+python main.py --sigma 9 --lambda_plus_sigma True
+python main.py --sigma 10 --lambda_plus_sigma True
+
+python main.py --sigma 1 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 2 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 3 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 4 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 5 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 6 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 7 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 8 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 9 --lambda_plus_sigma True --minus_percent_rate 10
+python main.py --sigma 10 --lambda_plus_sigma True --minus_percent_rate 10
